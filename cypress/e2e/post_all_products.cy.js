482bff75b6ff45de9dc46bc579f63c8e3f9f288b
reginaldocoutinho <reginaldo_re12@hotmail.com>
2026-01-22 14:49:45 -0300

    Enviada a requisição com o método POST, passando o usuário e senha no corpo da requisição

---
+++ cypress/e2e/post_all_products.cy.js
@@ -0,0 +1,16 @@
+it('POST To All Products List', () =>{
+    // Faz a requisição
+    cy.request({
+        method:'POST',
+        url:'https://automationexercise.com/api/productsList',
+        failOnStatusCode: false,//evita que o cypress falhe antes do teste
+        //corpo da requisição com as credências de um usuário cadastrado.
+        body:{email: 'teste3@outlook.com', password:'teste'}
+    }).then((response) => {
+        //Verifica o status code é o esperado
+        expect(response.status).to.eq(200);
+        //Verifica se a contem a mensagem no corpo da requisição
+        expect(response.body.responseCode).to.eq(405);
+        expect(response.body.message).to.eq("This request method is not supported.");
+    });
+});
