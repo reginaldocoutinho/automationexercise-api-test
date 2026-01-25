5173a92ab99180931f6b0fe04629f449acd69768
reginaldocoutinho <reginaldo_re12@hotmail.com>
2026-01-25 12:33:43 -0300

    Retornar o erro ao tentar atulizar sem passar parametros

--- cypress/e2e/put_to_all_brands.cy.js
+++ cypress/e2e/put_to_all_brands.cy.js
@@ -1,14 +1,15 @@
-describe('Testes de API - Automation Exercise', () => {
-  it('Deve validar que o método PUT não é suportado no endpoint brandsList', () => {
-    cy.request({
-      method: 'PUT',
-      url: 'https://automationexercise.com',
-      failOnStatusCode: false
-    }).then((response) => {
+// describe('Testes de API - Automation Exercise', () => {
+//   it('Deve validar que o método PUT não é suportado no endpoint brandsList', () => {
+//     cy.request({
+//       method: 'PUT',
+//       url: 'https://automationexercise.com',
+//       failOnStatusCode: false
+//     }).then((response) => {
       // O Cypress recebeu 200 na camada HTTP, então validamos o corpo da resposta
       // que contém o código 405 e a mensagem específica
-      expect(response.body.responseCode).to.eq(405);
-      expect(response.body.message).to.eq('This request method is not supported.');
-    });
-  });
-});
+      // expect(response.status).to.eq(403);
+      // expect(response.body.responseCode).to.eq(403);
+//       expect(response.body.message).to.eq('This request method is not supported.');
+//     });
+//   });
+// });
